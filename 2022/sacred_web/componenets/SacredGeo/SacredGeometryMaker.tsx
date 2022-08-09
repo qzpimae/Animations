c3736257b4c053e3af57e6c89e1cb9c751315332
qzpimae <yoshua.theg@gmail.com>
2022-08-09 00:05:43 -0400

    init sacred-web project

---
+++ 2022/sacred_web/componenets/SacredGeo/SacredGeometryMaker.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+    style?: React.CSSProperties,
+}
+
+const SacredGeometryMaker = (props: Props) => {
+  return (
+    <div style={props.style} >
+        Hello
+    </div>
+  )
+}
+
+export default SacredGeometryMaker
---
+++ 2022/sacred_web/pages/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import SacredGeometryMaker from '../componenets/SacredGeo/SacredGeometryMaker'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Sacred Geometry Generator" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SacredGeometryMaker />
+
+    </div>
+  )
+}
+
+export default Home
